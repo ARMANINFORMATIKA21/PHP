74b89edc1e56cce26c7f022b0ed4f5285170ed88
ARMANINFORMATIKA21 <armaninformatika21@gmail.com>
2021-12-25 06:08:21 +0800

    Belajar Operator Logika

---
+++ logika.py
@@ -0,0 +1,11 @@
+
+username = input("Username : ")
+password = input("Password : ")
+if username == "Arman" and password == "sembarang":
+	print("Login Sukses")
+else:
+	print("Username atau Password Invalid")
+
+
+
+
