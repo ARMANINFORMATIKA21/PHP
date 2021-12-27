2ce6e60e91b4b850f8c737d4b15b479da3045e24
ARMANINFORMATIKA21 <armaninformatika21@gmail.com>
2021-12-27 21:24:58 +0800

    php pemula

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype HTML>
+<html>
+  <body>
+    <form action = "tampil.php" method = "POST">
+      <label>Username</label>
+      <br>
+      <input name = "nama" type="text"/>
+      <br><label>Password</label><br>
+      <input name = "pw" type ="text"/><br>
+      <input type="submit" value="Kirim"/>
+    </form>
+  </body>
+ </html>
