a587a7e5609d7a07dff12c7544edd7a4d22f5992
AMMANK <96358041+ARMANINFORMATIKA21@users.noreply.github.com>
2021-12-27 21:52:45 +0800

    Delete list.py

--- list.py
+++
@@ -1,6 +0,0 @@
-hewan = []
-jumlah_list = int(input("Masukkan Jumlah list : "))
-for i in range(0,jumlah_list):
-	nama_hewan = input("Nama Hewan : ")
-	hewan.append(nama_hewan)
-print("hewan =",hewan)
