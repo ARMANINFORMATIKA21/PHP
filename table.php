f60df867261cec077620eb6add50734896199c26
AMMANK <96358041+ARMANINFORMATIKA21@users.noreply.github.com>
2022-01-02 21:05:05 +0800

    Update table.php

--- table.php
+++ table.php
@@ -5,7 +5,7 @@ $user = 'username';
 $pass = 'password';
 $db = 'nama_database';
 $connect = new mysqli($host,$user,$pass,$db);
-$sql = "CREATE TABLE username(id INT AUTO_INCREMENT primary key, name varchar(100))";
+$sql = "CREATE TABLE username(nama varchar(100),nim varchar(100))";
 if(mysqli_query($connect, $sql)){
   echo "Tabel Berhasil Dibuat";
 }
