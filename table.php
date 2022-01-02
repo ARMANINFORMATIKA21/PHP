187ebf484cfa31515c05b9824fd809fb2d5762f9
AMMANK <96358041+ARMANINFORMATIKA21@users.noreply.github.com>
2022-01-03 05:49:03 +0800

    Update table.php

--- table.php
+++ table.php
@@ -4,7 +4,7 @@ $host = '127.0.0.1';
 $user = 'username';
 $pass = 'password';
 $db = 'nama_database';
-$connect = new mysqli($host,$user,$pass,$db);
+$connect = mysqli_connect($host,$user,$pass,$db);
 $sql = "CREATE TABLE nama_table(nama varchar(100),nim varchar(100))";
 if(mysqli_query($connect, $sql)){
   echo "Tabel Berhasil Dibuat";
