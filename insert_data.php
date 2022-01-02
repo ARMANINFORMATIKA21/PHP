3365cabe5dbb79473d658af9c0cf03e9b4280212
AMMANK <96358041+ARMANINFORMATIKA21@users.noreply.github.com>
2022-01-02 21:00:00 +0800

    Update insert_data.php

--- insert_data.php
+++ insert_data.php
@@ -1,17 +1,18 @@
-
+//table.php
 <?php
-include'table.php';
-$nama = @$_POST['hewan'];
-$tombol = @$_POST['tombol'];
-
+$nama = $_POST['hewan'];
+$tombol = $_POST['tombol'];
+if(mysqli_query($connect, $sql)){
 $sql = "INSERT INTO nama_table (name) VALUES ('$nama')";
-$eks = mysqli_query($connect,$sql);
-
+$eksekusi = mysqli_query($connect,$sql);
+}
 ?>
+
+//index.php
 <!doctype HTML>
 <html>
    <body>
-      <form action = '' method = 'POST'/>
+      <form action = 'table.php' method = 'POST'/>
       <input name = 'hewan' type = 'text'/>
       <button type = 'submit' name = 'tombol'>upload</button>
    </body>
